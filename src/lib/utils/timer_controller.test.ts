d6a71280f6428ee19ce9bccc223f0c6ba5c030a4
blorbb <88137137+blorbb@users.noreply.github.com>
2023-02-03 18:45:49 +1100

    refactor: change from it() to test()

--- src/lib/utils/timer_controller.test.ts
+++ src/lib/utils/timer_controller.test.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
 import { TimerController } from "./timer_controller";
 
 describe("Parses time", () => {
 	describe("From milliseconds to h/m/s/ms", () => {
-		it.each([
+		test.each([
 			[100, 0, 0, 0, 100],
 			[58000, 0, 0, 58, 0],
 			[60000, 0, 1, 0, 0],
@@ -15,7 +15,7 @@ describe("Parses time", () => {
 			expect(TimerController.parseToUnits(time)).toEqual({ h, m, s, ms });
 		});
 
-		it.each([
+		test.each([
 			[-100, -0, -0, -0, -100],
 			[-58000, -0, -0, -58, -0],
 			[-60000, -0, -1, -0, -0],
@@ -32,53 +32,53 @@ describe("Parses time", () => {
 
 	describe("From milliseconds to a clock time", () => {
 		describe("Handles trims and padding", () => {
-			it("Produces correct time range", () => {
+			test("Produces correct time range", () => {
 				expect(TimerController.parseToClock(137020, ["ms", "m"])).toEqual(
 					"2:17.020",
 				);
 			});
 
-			it("Can swap the given time range", () => {
+			test("Can swap the given time range", () => {
 				expect(TimerController.parseToClock(137020, ["m", "ms"])).toEqual(
 					"2:17.020",
 				);
 			});
 
-			it("Can convert large units into the given range", () => {
+			test("Can convert large units into the given range", () => {
 				expect(TimerController.parseToClock(5315938, ["ms", "m"])).toEqual(
 					"88:35.938",
 				);
 			});
 
-			it("Can allow units to have a longer string when LHS is trimmed", () => {
+			test("Can allow units to have a longer string when LHS is trimmed", () => {
 				expect(TimerController.parseToClock(325225, ["ms", "s"])).toEqual(
 					"325.225",
 				);
 			});
 
-			it("Adds necessary 0 padding", () => {
+			test("Adds necessary 0 padding", () => {
 				expect(TimerController.parseToClock(123456, ["ms", "h"])).toEqual(
 					"0:02:03.456",
 				);
 			});
 
-			it("Provides a default time range", () => {
+			test("Provides a default time range", () => {
 				expect(TimerController.parseToClock(2040)).toEqual("0:00:02.040");
 			});
 
-			it("Adds 1 to rightmost unit when positive", () => {
+			test("Adds 1 to rightmost unit when positive", () => {
 				expect(TimerController.parseToClock(137020, ["s", "m"])).toEqual(
 					"2:18",
 				);
 			});
 
-			it("Does not add 1 when RHS unit is 0", () => {
+			test("Does not add 1 when RHS unit is 0", () => {
 				expect(TimerController.parseToClock(137000, ["s", "m"])).toEqual(
 					"2:17",
 				);
 			});
 
-			it("Supports single units, not adding separators", () => {
+			test("Supports single units, not adding separators", () => {
 				expect(TimerController.parseToClock(3242521, ["s", "s"])).toEqual(
 					"3243",
 				);
@@ -86,21 +86,21 @@ describe("Parses time", () => {
 		});
 
 		describe("Handles negative times", () => {
-			it("Adds minus sign when negative", () => {
+			test("Adds minus sign when negative", () => {
 				expect(TimerController.parseToClock(-19394)).toEqual("-0:00:19.394");
 			});
 
-			it("Shifts minus sign when LHS is trimmed", () => {
+			test("Shifts minus sign when LHS is trimmed", () => {
 				expect(TimerController.parseToClock(-38471, ["ms", "m"])).toEqual(
 					"-0:38.471",
 				);
 			});
 
-			it("When trimmed time is 0, still recognises as negative", () => {
+			test("When trimmed time is 0, still recognises as negative", () => {
 				expect(TimerController.parseToClock(-230, ["s", "m"])).toEqual("-0:00");
 			});
 
-			it("Does not add 1 to time when negative", () => {
+			test("Does not add 1 to time when negative", () => {
 				expect(TimerController.parseToClock(-23423, ["s", "m"])).toEqual(
 					"-0:23",
 				);
@@ -157,22 +157,22 @@ describe("Can run", () => {
 			expect(timer[funcName]()).toEqual(values[5]);
 		}
 
-		it("is started", () => {
+		test("is started", () => {
 			const timer = new TimerController(1000);
 			results(timer, "isStarted", [false, true, true, true, true, false]);
 		});
 
-		it("is paused", () => {
+		test("is paused", () => {
 			const timer = new TimerController(1000);
 			results(timer, "isPaused", [false, false, true, false, false, false]);
 		});
 
-		it("is running", () => {
+		test("is running", () => {
 			const timer = new TimerController(1000);
 			results(timer, "isRunning", [false, true, false, true, false, false]);
 		});
 
-		it("is stopped", () => {
+		test("is stopped", () => {
 			const timer = new TimerController(1000);
 			results(timer, "isStopped", [false, false, false, false, true, false]);
 		});
