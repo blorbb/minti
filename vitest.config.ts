59d35c58dde0d202e2fe71d86f5cf3f0e4249b7f
blorbb <88137137+blorbb@users.noreply.github.com>
2023-02-04 16:43:40 +1100

    build: add vitest coverage tests, add pnpm scripts
    the pnpm script `test:unit` renamed to `test`, `coverage` script added

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,12 @@ export default defineConfig({
 	define: {
 		"import.meta.vitest": "undefined",
 	},
+	test: {
+		coverage: {
+			provider: "c8",
+			reporter: ["text", "json", "html"],
+		},
+	},
 	// test: {
 	// 	includeSource: ["src/**/*.{js,ts}"],
 	// },
