ace11f63d95235c162acff5d415ab144d37c3e16
blorbb <88137137+blorbb@users.noreply.github.com>
2023-02-19 16:22:22 +1100

    build: fix eslint giving incorrect errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,11 @@ module.exports = {
 	],
 	plugins: ["svelte3", "@typescript-eslint"],
 	ignorePatterns: ["*.cjs"],
-	overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
+	overrides: [
+		{ files: ["*.svelte"], processor: "svelte3/svelte3" },
+		// https://github.com/Chatie/eslint-config/issues/45#issuecomment-1003990077
+		{ files: ["*.svelte", "*.ts"], rules: { "no-undef": "off" } },
+	],
 	settings: {
 		"svelte3/typescript": () => require("typescript"),
 	},
