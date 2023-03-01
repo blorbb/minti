477356cd24fdf6aa859381d5717b7c2e232cc1f7
blorbb <88137137+blorbb@users.noreply.github.com>
2023-03-01 17:21:28 +1100

    feat(ui): add overscroll shadows + remove scrollbars

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,11 +10,45 @@
 	import NavBar from "$lib/components/NavBar.svelte";
 	import { setCSSProp } from "$lib/utils/css";
 	import { settings } from "$lib/utils/stores";
+	import { onMount } from "svelte";
 
 	setCSSProp(
 		"--l-progress-bar--bg__border-width",
 		$settings.progressBarBackgroundBorder.toString() + "px",
 	);
+
+	// show an overflow shadow
+	// https://cushionapp.com/journal/overflow-shadows
+	let contextElem: HTMLElement;
+	let topShadowElem: HTMLDivElement;
+	let bottomShadowElem: HTMLDivElement;
+	let topEdge: HTMLDivElement;
+	let bottomEdge: HTMLDivElement;
+
+	onMount(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				console.log(entries);
+
+				for (const entry of entries) {
+					const edge = (entry.target as HTMLElement).dataset["side"];
+					const shadowToShow =
+						edge === "top" ? topShadowElem : bottomShadowElem;
+
+					if (entry.isIntersecting) {
+						shadowToShow.style.opacity = "0";
+					} else {
+						shadowToShow.style.opacity = "1";
+					}
+				}
+			},
+			{
+				root: contextElem,
+			},
+		);
+		observer.observe(topEdge);
+		observer.observe(bottomEdge);
+	});
 </script>
 
 <svelte:head>
@@ -23,15 +57,58 @@
 </svelte:head>
 
 <div class="viewport">
-	<div class="context">
+	<div class="context" bind:this={contextElem}>
+		<div class="top-shadow scroll-shadow" bind:this={topShadowElem} />
 		<main>
+			<div class="intersection-edge" data-side="top" bind:this={topEdge} />
 			<slot />
+			<div
+				class="intersection-edge"
+				data-side="bottom"
+				bind:this={bottomEdge}
+			/>
 		</main>
+		<div class="bottom-shadow scroll-shadow" bind:this={bottomShadowElem} />
 	</div>
 	<NavBar />
 </div>
 
 <style lang="scss">
+	.scroll-shadow {
+		// TODO make shadow version customisable in settings
+		// dark shadow
+		// --s-shadow-color: hsla(0, 0%, 0%, 0.5);
+		// --s-shadow-height: 1rem;
+		// mix-blend-mode: multiply;
+
+		// light shadow
+		--s-shadow-color: hsla(184, 100%, 97%, 0.1);
+		--s-shadow-height: 0.5rem;
+		mix-blend-mode: screen;
+
+		position: fixed;
+		left: 0;
+		right: 0;
+
+		height: var(--s-shadow-height);
+
+		transition: opacity var(--t-transition--short) linear;
+
+		&.top-shadow {
+			top: 0;
+			background: linear-gradient(var(--s-shadow-color), transparent);
+		}
+
+		&.bottom-shadow {
+			bottom: 0;
+			background: linear-gradient(transparent, var(--s-shadow-color));
+		}
+	}
+
+	main {
+		position: relative;
+	}
+
 	.viewport {
 		display: flex;
 		flex-direction: column;
@@ -45,6 +122,8 @@
 		position: relative;
 		overflow: hidden;
 
+		isolation: isolate;
+
 		// required to make `position: fixed;` relative to this.
 		// using `position: absolute;` doesn't work with scrolling
 		// https://stackoverflow.com/a/38796408
@@ -54,6 +133,11 @@
 			overflow-y: scroll;
 			width: 100%;
 			height: 100%;
+			z-index: 0;
+		}
+
+		> .scroll-shadow {
+			z-index: 1;
 		}
 	}
 
