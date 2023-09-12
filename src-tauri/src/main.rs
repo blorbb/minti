4202fd677b4d9136726d7c78bc01b08f685f94ec
blorbb <88137137+blorbb@users.noreply.github.com>
2023-09-12 23:28:38 +1000

    back to tauri 1

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn alert_window(window: tauri::Window) {
+    window
+        .request_user_attention(Some(tauri::UserAttentionType::Critical))
+        .expect("should be able to request user attention");
+}
+
 fn main() {
-    #[cfg(desktop)]
-    minti_lib::run();
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![alert_window])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
