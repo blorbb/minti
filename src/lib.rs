95ec8d48b16d9c430f061e02a0a6b041136a96e3
blorbb <88137137+blorbb@users.noreply.github.com>
2024-06-12 13:36:50 +1000

    add console dbg macro

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,30 @@ pub mod interpreter;
 pub mod reactive;
 pub mod time;
 pub mod timer;
+
+#[allow(unused_macros)]
+macro_rules! wdbg {
+    // NOTE: We cannot use `concat!` to make a static string as a format argument
+    // of `eprintln!` because `file!` could contain a `{` or
+    // `$val` expression could be a block (`{ .. }`), in which case the `eprintln!`
+    // will be malformed.
+    () => {
+        ::log::warn!("[{}:{}:{}]", ::std::file!(), ::std::line!(), ::std::column!())
+    };
+    ($val:expr $(,)?) => {
+        // Use of `match` here is intentional because it affects the lifetimes
+        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+        match $val {
+            tmp => {
+                ::log::warn!("[{}:{}:{}] {} = {:#?}",
+                    ::std::file!(), ::std::line!(), ::std::column!(), ::std::stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::wdbg!($val)),+,)
+    };
+}
+#[allow(unused_imports)]
+pub(crate) use wdbg;
